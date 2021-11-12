ff89f4977e89a637d1f1e171e2d8ed824aca97be
Stanislav <n.noiser@gmail.com>
2021-11-12 23:02:47 +0300

    lesson 6 Lists

--- snake/Program.cs
+++ snake/Program.cs
@@ -11,8 +11,31 @@ namespace snake
         static void Main(string[] args)
         {
             Point p1 = new Point(1,3,'%');
+            Point p2 = new Point(3, 5, '@');
 
             p1.draw();
+
+            List<int> numList = new List<int>();
+            numList.Add(0);
+            numList.Add(1);
+            numList.Add(2);
+
+            int x = numList[0];
+            int y = numList[1]; 
+            int z = numList[2];
+
+            foreach (int i in numList)
+            {
+                Console.WriteLine(i);   
+            }
+
+            numList.RemoveAt(0);
+
+            List<Point> pList = new List<Point>(); 
+            pList.Add(p1);
+            pList.Add(p2);
+
+
 
             Console.ReadLine();
         }
