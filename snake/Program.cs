c591a391bb0d49cd91f4a12b729d0d1597b12423
Stanislav <n.noiser@gmail.com>
2021-11-12 21:00:26 +0300

    2021-11-12 Classes

--- snake/Program.cs
+++ snake/Program.cs
@@ -10,21 +10,13 @@ namespace snake
     {
         static void Main(string[] args)
         {
-            int x1 = 1;
-            int y1 = 3;
-            char sym1 = '*';
+            Point p1 = new Point();
+            p1.x = 1;
+            p1.y = 5;
+            p1.sym = '#';
 
-
-            Draw(x1, y1, sym1);
+            p1.draw();
             Console.ReadLine();
-
-        }
-
-        static void Draw(int x, int y, char sym)
-        {
-            Console.SetCursorPosition(x, y);
-            Console.Write(sym);
-
         }
     }
 }
