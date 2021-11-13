ae17e24797aba3a5bf6ba5273b5b7cf2c0a77e78
Stanislav <n.noiser@gmail.com>
2021-11-13 17:51:18 +0300

    lesson 9 Class Snake

--- snake/Program.cs
+++ snake/Program.cs
@@ -16,13 +16,17 @@ namespace snake
             //Drawing frame
             HorizontalLine Topline = new HorizontalLine(0, 78, 0, '+');
             HorizontalLine Bottomline = new HorizontalLine(0, 78, 24, '+');
-            VerticalLine LeftLine = new VerticalLine(0, 24, 0, '+');
+            VerticalLine LeftLine = new VerticalLine(0, 24, 0, '+'); 
             VerticalLine RightLine = new VerticalLine(0, 24, 78, '+');
 
             Topline.Drow();
             Bottomline.Drow();
             LeftLine.Drow();    
-            RightLine.Drow();   
+            RightLine.Drow();
+
+            Point p = new Point(3, 3, '*');
+            Snake snake = new Snake(p, 4, Direction.RIGHT);
+            snake.Drow();
 
             Console.ReadLine();
         }
